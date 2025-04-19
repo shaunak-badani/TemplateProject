c06c2444933a45a1dfd2bc9c1d17a023a88f7746
Shaunak Badani <shaunak.badani@duke.edu>
2025-04-18 22:32:14 -0400

    Added dismiss button

--- frontend/src/model-cards/error-overlay.tsx
+++ frontend/src/model-cards/error-overlay.tsx
@@ -1,5 +1,6 @@
 import useGlobalStore from "@/store/store";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const ErrorOverlay = () => {
   const error = useGlobalStore((state) => state.error);
@@ -9,6 +10,12 @@ const ErrorOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
       <Card className="max-w-md p-6 bg-red-600  shadow-xl">
+        <Button
+            onClick={() => useGlobalStore.getState().setError(null)}
+            className="mt-4 text-sm text-blue-600 hover:underline"
+            >
+            Dismiss
+        </Button>
         <CardContent className="text-white">
           <h2 className="text-2xl font-bold mb-4">Error</h2>
           <p>{error}</p>
