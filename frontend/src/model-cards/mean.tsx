f1283bdbfb7407697bfe9b6534822b8b3741f310
Shaunak Badani <shaunak.badani@duke.edu>
2025-04-18 21:18:05 -0400

    Added select dropdown for selecting some preset values

--- frontend/src/model-cards/mean.tsx
+++ frontend/src/model-cards/mean.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import BackdropWithSpinner from "@/components/ui/backdropwithspinner";
 import backendClient from "@/backendClient";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 
 const Mean = () => {
@@ -10,6 +11,11 @@ const Mean = () => {
     const [isLoading, setLoading] = useState(false);
     const [query, setQuery] = useState("");
     const [response, setResponse] = useState("");
+
+    const presetValues = [
+        "What is the weather in New york today?",
+        "What is 2 + 2?"
+    ]
 
     const handlePromptInput = async(query: string) => {
         setLoading(true);
@@ -29,6 +35,23 @@ const Mean = () => {
                 value={query}
                 onChange = {(e) => setQuery(e.target.value)} 
                 placeholder="Enter your query here!" />
+            <div className="my-6">
+                <Select onValueChange={setQuery}>
+                    <SelectTrigger id="querySelector">
+                    <SelectValue placeholder="Choose a sample query from the presets.." />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                    {presetValues.map((preset, index) => 
+                        (<SelectItem
+                            key={index}
+                            value={preset} 
+                            >
+                            {preset}
+                        </SelectItem>)
+                    )}
+                    </SelectContent>
+                </Select>
+            </div>
             <Button className="p-6 sm:p-6 rounded-2xl m-8 sm:m-8" onClick={() => handlePromptInput(query)}>
                 Send
             </Button>
